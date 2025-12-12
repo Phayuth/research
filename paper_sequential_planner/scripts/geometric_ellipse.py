2684e4bf634ed8287ce14d5613aba20d2a7887cc
yuth <yuth@todo.com>
2025-12-12 17:43:58 +0900

    u

--- paper_sequential_planner/scripts/geometric_ellipse.py
+++ paper_sequential_planner/scripts/geometric_ellipse.py
@@ -13,6 +13,13 @@ def informed_sampling(xCenter, cMax, cMin, rotationAxisC):
     return xRand
 
 
+def informed_surface_sampling(xCenter, cMax, cMin, rotationAxisC):
+    L = hyperellipsoid_axis_length(cMax, cMin)
+    xBall = unit_ball_surface_sampling()
+    xRand = (rotationAxisC @ L @ xBall) + xCenter
+    return xRand
+
+
 def hyperellipsoid_axis_length(cMax, cMin, dof=2):  # L
     r1 = cMax / 2
     ri = np.sqrt(cMax**2 - cMin**2) / 2
@@ -25,6 +32,13 @@ def unit_ball_sampling(dof=2):
     norm = np.linalg.norm(u)
     u = u / norm
     return u[:dof, :]  # The first N coordinates are uniform in a unit N ball
+
+
+def unit_ball_surface_sampling(dof=2):
+    u = np.random.normal(0.0, 1.0, (dof, 1))
+    norm = np.linalg.norm(u)
+    u = u / norm
+    return u  # The first N coordinates are uniform on the surface of a unit N ball
 
 
 def rotation_to_world(xStart, xGoal):  # C
@@ -71,16 +85,27 @@ def __usage():
     cMin = distance_between_config(xStart, xGoal)
     cMax = 2.0
     XRAND = [informed_sampling(xCenter, cMax, cMin, C) for _ in range(1000)]
+    XRAND = np.array(XRAND).reshape(-1, dof)
+    XRAND_surface = [
+        informed_surface_sampling(xCenter, cMax, cMin, C) for _ in range(1000)
+    ]
+    XRAND_surface = np.array(XRAND_surface).reshape(-1, dof)
 
     # plot
-    fig, (ax, ay) = plt.subplots(1, 2)
+    fig, (ax, ay, az) = plt.subplots(1, 3)
 
     # fig1
     ax.plot(xStart[0], xStart[1], marker="o", color="blue", label="Start")
     ax.plot(xGoal[0], xGoal[1], marker="o", color="green", label="Goal")
     ax.plot(xCenter[0], xCenter[1], marker="x", color="black", label="Center")
-    for x in XRAND:
-        ax.plot(x[0], x[1], marker=".", color="gray", alpha=0.5)
+    ax.plot(
+        XRAND[:, 0],
+        XRAND[:, 1],
+        marker=".",
+        linestyle="None",
+        color="gray",
+        alpha=0.5,
+    )
     el = get_2d_ellipse_mplpatch(xStart, xGoal, cMax, cMin)
     ax.add_patch(el)
     ax.set_aspect("equal", "box")
@@ -93,17 +118,37 @@ def __usage():
     ay.plot(xStart[0], xStart[1], marker="o", color="blue", label="Start")
     ay.plot(xGoal[0], xGoal[1], marker="o", color="green", label="Goal")
     ay.plot(xCenter[0], xCenter[1], marker="x", color="black", label="Center")
-    CMAX = np.linspace(cMin, cMin * 5, 10)
-    for cmax in CMAX:
-        el = get_2d_ellipse_mplpatch(xStart, xGoal, cmax, cMin)
-        ay.add_patch(el)
-        fs = f"cMax is +{((cmax - cMin) / cMin) * 100:.1f}% of cMin"
-        ay.plot([], [], label=fs)  # dummy plot for legend
+    ay.plot(
+        XRAND_surface[:, 0],
+        XRAND_surface[:, 1],
+        marker=".",
+        linestyle="None",
+        color="gray",
+        alpha=0.5,
+    )
+    el = get_2d_ellipse_mplpatch(xStart, xGoal, cMax, cMin)
+    ay.add_patch(el)
     ay.set_aspect("equal", "box")
     ay.set_xlim(-np.pi, np.pi)
     ay.set_ylim(-np.pi, np.pi)
     ay.grid(True)
     ay.legend()
+
+    # fig3
+    az.plot(xStart[0], xStart[1], marker="o", color="blue", label="Start")
+    az.plot(xGoal[0], xGoal[1], marker="o", color="green", label="Goal")
+    az.plot(xCenter[0], xCenter[1], marker="x", color="black", label="Center")
+    CMAX = np.linspace(cMin, cMin * 5, 10)
+    for cmax in CMAX:
+        el = get_2d_ellipse_mplpatch(xStart, xGoal, cmax, cMin)
+        az.add_patch(el)
+        fs = f"cMax is +{((cmax - cMin) / cMin) * 100:.1f}% of cMin"
+        az.plot([], [], label=fs)  # dummy plot for legend
+    az.set_aspect("equal", "box")
+    az.set_xlim(-np.pi, np.pi)
+    az.set_ylim(-np.pi, np.pi)
+    az.grid(True)
+    az.legend()
     plt.show()
 
 
